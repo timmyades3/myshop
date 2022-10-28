02c2e983b57cfd613e29406e01772b2e26708a5b
timmyades3 <oluwatimileyinadesina623@gmail.com>
2022-10-28 21:58:16 +0100

    second commit

--- pyshop/pyshop/settings.py
+++ pyshop/pyshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('pyshop')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
